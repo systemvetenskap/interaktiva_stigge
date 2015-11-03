ffc3159686f7b89f1bbe720ebf4f5e6df92988a4
Jacob Hyllengren Larsson <derhyllengren@hotmail.com>
2015-11-03 20:12:20 +0100

    dfdf

--- bankprov/bankprov/index.aspx.cs
+++ bankprov/bankprov/index.aspx.cs
@@ -6,109 +6,132 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Npgsql;
 using NpgsqlTypes;
+using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace bankprov
 {
     public partial class index : System.Web.UI.Page
     {
+        public string anv = "jahy1400";
+        public bool provledare = true;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            btnLamnain.Visible = false;
+            arlinsensierad();
+            //LiteralFraga.Visible = false;
+            //LabelA.Visible = false;
+            //LabelB.Visible = false;
+            //LabelC.Visible = false;
+            //LabelD.Visible = false;
+            //RadioButtonA.Visible = false;
+            //RadioButtonB.Visible = false;
+            //RadioButtonC.Visible = false;
+            //RadioButtonD.Visible = false;
+
 
         }
 
-        public static int GetPersonId(string anvandare)
+        public void arlinsensierad()
         {
-            //a connectionString = "Server=webblabb.miun.se;Port=5432;Database=pgmvaru_g1;User Id=pgmvaru_g1;Password=enhjuling;SSL=true";
-            string connectionString = "Server=webblabb.miun.se; Port=5432; Database=pgmvaru_g8; User Id=pgmvaru_g8; Password=rockring; SslMode=Require; Trust Server Certificate=true";
+            if (anv == "jahy1400" && provledare == true)
+            {
+                btnGorProv.Visible = true;
+                btnSeResultat.Visible = true;
+                btnSeResultatAnstallda.Visible = true;
+                LabelEjInloggad.Visible = false;
+                btnLamnain.Visible = false;
 
-            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
-            int person_id = 0;
-            try
-            {
-                conn.Open();
-                string sql = "SELECT fk_person_id FROM u4_konto WHERE anvandarnamn = :anvandare;";
-                NpgsqlCommand command = new NpgsqlCommand(@sql, conn);
-                command.Parameters.Add(new NpgsqlParameter("anvandare", NpgsqlDbType.Varchar));
-                command.Parameters["anvandare"].Value = anvandare;
-                NpgsqlDataReader dr = command.ExecuteReader();
-                while (dr.Read())
-                {
-                    person_id = Convert.ToInt32(dr["fk_person_id"]);
-                }
-            }
-            catch (NpgsqlException ex)
-            {
-            }
-            finally
-            {
-                conn.Close();
-            }
-            return person_id;
-        }
-        public static bool arlinsensierad(int fk_person_id)
-        {
-            string connectionString = "Server=webblabb.miun.se; Port=5432; Database=pgmvaru_g8; User Id=pgmvaru_g8; Password=rockring; SslMode=Require; Trust Server Certificate=true";
-            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
-            bool linsensierad = false;
-            try
-            {
-                conn.Open();
-                string sql = "SELECT linsensierad FROM u4_person WHERE id = :fk_person_id;";
-                NpgsqlCommand command = new NpgsqlCommand(@sql, conn);
-                command.Parameters.Add(new NpgsqlParameter("fk_person_id", NpgsqlDbType.Varchar));
-                command.Parameters["fk_person_id"].Value = fk_person_id;
-                NpgsqlDataReader dr = command.ExecuteReader();
-                while (dr.Read())
-                {
-                    linsensierad = (bool)(dr["linsensierad"]);
-                }
-            }
-            catch (NpgsqlException ex)
-            {
-            }
-            finally
-            {
-                conn.Close();
-            }
-            return linsensierad;
-        }
 
-        protected void ButtonPersonal_Click(object sender, EventArgs e)
-        {
-            string anvandare = TextBoxanvandare.Text;
-            int person_id = 0;
-            person_id = GetPersonId(anvandare);
-            string provtip;
-            if (arlinsensierad(person_id) == true)
-            {
-                //öppna sidan för kunskapsprov
-                provtip = "kunskapsprov";
             }
+
+            else if (anv == "jahy1400" && provledare == false)
+            {
+                btnGorProv.Visible = true;
+                btnSeResultat.Visible = true;
+                btnSeResultatAnstallda.Visible = false;
+                LabelEjInloggad.Visible = false;
+                btnLamnain.Visible = false;
+
+
+            }
+
             else
             {
-                //öppna sidan för linsensiering
-                provtip = "lisensiering";
-            }
+                btnGorProv.Visible = false;
+                btnSeResultat.Visible = false;
+                btnSeResultatAnstallda.Visible = false;
+                LabelEjInloggad.Visible = true;
+                LabelEjInloggad.Text = "Du måste vara inloggad för att använda kompetensportalen";
+                btnLamnain.Visible = false;
 
+            }
 
         }
 
-        protected void ButtonProvledare_Click(object sender, EventArgs e)
+        protected void btnGorProv_Click(object sender, EventArgs e)
         {
-            string anvandare = TextBoxprovledare.Text;
-            int person_id = 0;
-            person_id = GetPersonId(anvandare);
-            string provtip;
-            if (arlinsensierad(person_id) == true)
+            btnGorProv.Visible = false;
+            btnSeResultat.Visible = false;
+            btnSeResultatAnstallda.Visible = false;
+            LabelEjInloggad.Visible = false;
+            LabelKompetensportal.Visible = false;
+            btnLamnain.Visible = true;
+
+
+            HamtaFragor();
+
+            //LiteralFraga.Visible = true;
+            //LabelA.Visible = true;
+            //LabelB.Visible = true;
+            //LabelC.Visible = true;
+            //LabelD.Visible = true;
+            //RadioButtonA.Visible = true;
+            //RadioButtonB.Visible = true;
+            //RadioButtonC.Visible = true;
+            //RadioButtonD.Visible = true;
+
+
+        }
+
+        public void HamtaFragor()
+        {
+            string xml = Server.MapPath("test.xml");
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(prov));
+            TextReader reader = new StreamReader(xml);
+            object obj = deserializer.Deserialize(reader);
+            prov XmlData = (prov)obj;
+            reader.Close();
+
+
+
+            Repeater1.DataSource = XmlData.fragelista;
+            Repeater1.DataBind();
+
+        }
+
+        protected void btnLamnain_Click(object sender, EventArgs e)
+        {
+            foreach (RepeaterItem item in Repeater1.Items)
             {
-                //öppna sidan för kunskapsprov
-                provtip = "kunskapsprov";
-            }
-            else
-            {
-                //öppna sidan för linsensiering
-                provtip = "lisensiering";
+                // Checking the item is a data item
+                if (item.ItemType == ListItemType.Item || item.ItemType == ListItemType.AlternatingItem)
+                {
+                    var rdbList = item.FindControl("RadioButtonList1") as RadioButtonList;
+                    // Get the selected value
+                    string selected = rdbList.SelectedValue;
+                }
             }
         }
+
+
+
+
     }
 }
+
