2ae4e365b28f9cf2678f5919074ed3c8cc18a278
enke1200 <enke1200@student.miun.se>
2015-11-19 23:10:45 +0100

    lite ändringar

---
+++ bankprov/bankprov/Anvandare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+
+namespace bankprov
+{
+    public class Anvandare
+    {
+        public int person_id;
+        public string fnamn;
+        public string enamn;
+
+        public override string ToString()
+        {
+            return fnamn + " " + enamn;
+        }
+    }
+}
